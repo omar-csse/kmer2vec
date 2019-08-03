023c7769ae7ce4b3223743217874ddf128d52f04
omar <omar.csse@gmail.com>
2019-08-04 07:03:22 +1000

    handle window reload

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,16 @@
 let coordsChart = [];
 let sequences = []
 let currentIndex = 0;
+let chart;
+
+window.onload = (e) => {
+    let addedSequences = document.getElementsByClassName("addedSequences")[0]
+    const storedItems = [...Object.keys(localStorage)]
+    for (let i = 0; i < storedItems.length; i++) {
+        let html = JSON.parse(localStorage.getItem(storedItems[i]))
+        addedSequences.innerHTML += html
+    }
+}
 
 const random_rgba = () => {
     let o = Math.round
@@ -39,7 +49,7 @@ const fetchData = async () => {
 const drawchart = () => {
 
     // For a bubble chart
-    let chart = new Chart(document.getElementById("chart").getContext('2d'), {
+    chart = new Chart(document.getElementById("chart").getContext('2d'), {
         type: 'bubble',
         data: {
             datasets: coordsChart
@@ -80,16 +90,15 @@ const viewBtn = () => {
 
     if (chart.style.display === "none") {
         viewbtn.textContent = "Table View"
+        table.style.display = 'none'
+        chart.style.display = '';
         document.getElementById("table-wrapper").style.height = "0px"
-        table.innerHTML = ''
-        chart.style.display = "block";
-        document.getElementsByClassName("chart-container")[0].style.display = "block"
     } else {
         viewbtn.textContent = "Chart View"
         chart.style.display = "none";
-        document.getElementsByClassName("chart-container")[0].style.display = "none"
+        table.style.display = ''
         document.getElementById("table-wrapper").style.height = "500px"
-
+        currentIndex = 0;
         showSequences(5);
     }   
 }
@@ -98,7 +107,7 @@ const loadSequences = () => {
     let table = document.getElementById("sequencesTable");
     tr = table.getElementsByTagName("tr");
     for (let i = 0; i < sequences.length; i++) {
-        let html = `Id: <span id="sequence${i}">${sequences[i].promoter_id}</span>\n<div class="sequence">Sequence: ${sequences[i].promoter_sequence}</div>\nx: ${sequences[i].x}\ny: ${sequences[i].y}\nMean of the vector: ${sequences[i].mean}`;
+        let html = `Id: <span id="sequence-${sequences[i].promoter_id}">${sequences[i].promoter_id}</span>\n<div class="sequence">Sequence: ${sequences[i].promoter_sequence}</div>\nx: ${sequences[i].x}\ny: ${sequences[i].y}\nMean of the vector: ${sequences[i].mean}`;
         let newRow = table.insertRow(-1);
         let newCell = newRow.insertCell(0).innerHTML = html;
         tr[i].style.display = "none"
@@ -107,14 +116,14 @@ const loadSequences = () => {
 
 const showSequences = (num) => {
     let table = document.getElementById("sequencesTable");
-    tr = table.getElementsByTagName("tr");
+    let tr = table.getElementsByTagName("tr");
     for (let i = currentIndex; i < currentIndex+num; i++) {
         tr[i].style.display = ""
     }
     currentIndex += num 
 }
 
-const onscrollingSequencesTable = () => {
+const onScrollingSequencesTable = () => {
     tableScroll = document.getElementById("table-wrapper")
     input = document.getElementById("search-box").value
     if (input == '') {
@@ -122,6 +131,39 @@ const onscrollingSequencesTable = () => {
             showSequences(5);
         }
     }
+}
+
+const onTyping = (e) => {
+    let viewBtn = document.getElementById("view-btn");
+    if (viewBtn.textContent == "Chart View") filterTable(e);
+}
+
+const clearSearch = () => {
+    document.getElementById('search-box').value = ''
+    for (i = 0; i < tr.length; i++) tr[i].style.display = "none"
+    let viewBtn = document.getElementById("view-btn");
+    if (viewBtn.textContent == "Chart View") {
+        currentIndex = 0;
+        showSequences(5);
+    } 
+    document.getElementsByClassName("addedSequences")[0].innerHTML = '';
+    localStorage.clear()
+}
+
+const addSequence = () => {
+    let desiredSequence = document.getElementById('search-box').value
+    desiredSequence = sequences.filter(seq => seq.promoter_id == desiredSequence)
+    if (localStorage.getItem(desiredSequence[0].promoter_id) === null) { 
+        let addedSequences = document.getElementsByClassName("addedSequences")[0]
+        html = `<div id="addedSeq-${desiredSequence[0].promoter_id}" class="addedSeq" onclick="removeSequence('${desiredSequence[0].promoter_id}');">Id: ${desiredSequence[0].promoter_id} - Mean: ${desiredSequence[0].mean}</div>`
+        addedSequences.innerHTML += html
+        localStorage.setItem(desiredSequence[0].promoter_id, JSON.stringify(html))
+    }
+}
+
+const removeSequence = (seq) => {
+    localStorage.removeItem(seq)
+    document.getElementById(`addedSeq-${seq}`).remove()
 }
 
 const filterTable = (e) => {
@@ -136,9 +178,7 @@ const filterTable = (e) => {
             sequence.toLowerCase().indexOf(term) > -1 ? tr[i].style.display = "" : tr[i].style.display = "none"   
         }
     } else {
-        for (i = 0; i < tr.length; i++) tr[i].style.display = "none"
-        currentIndex = 0;
-        showSequences(5)
+        clearSearch()
     }
 }
 
