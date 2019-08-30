89f114112c47927e9efa385bdaa301513f24a56d
omar <omar.csse@gmail.com>
2019-08-30 19:57:09 +1000

    seperate api functions in model.js file

--- routes/api/api.sequences.js
+++ routes/api/api.sequences.js
@@ -1,50 +1,26 @@
-const data = require('../../lib/data/sequence_coords')
-const api = require('./sequences.json')
+const api = require('./models')
 const errors = require('./errors.json')
 const router = require('express').Router()
-const num_sequences = data.sequences.length
 
 router.get('/api/sequences', (req, res) => {
-    res.json(getSequences(num_sequences));
+    res.json(api.getSequences());
 })
 
 router.get('/api/sequences/:id', (req, res) => {
     if (req.params.id < 1 || req.params.id > 10) res.send(errors.out_of_range_error).end(404)
-    else res.json(getSequencesPage(req.params.id));
+    else res.json(api.getSequencesPage(req.params.id));
 })
 
-router.get('/api/sequence/:id', (req, res) => {
+router.get('/api/sequence/:id', async (req, res) => {
 
     if (!req.params.id.match("^[a-zA-Z0-9]+")) {
         res.send(errors.sequence_not_found).end(404)
     } else {
-        sequence = getSequenceById(req.params.id.toUpperCase())
+        sequence = await api.getSequenceById(req.params.id.toUpperCase())
         if (Object.keys(sequence.data.sequence).length > 0) res.json(sequence);
         else res.send(errors.sequence_not_found).end(404)
     }
 
 })
-
-const getSequences = (max) => {
-    api.data.number_of_sequences = max
-    api.data.sequences = data.sequences.slice(0, max)
-    return api
-}
-
-const getSequencesPage = (page) => {
-
-    if (page == 1) api.data.sequences = data.sequences.slice( 0, page*200)
-    else if (page == 10) api.data.sequences = data.sequences.slice( (page-1) *200)
-    else api.data.sequences = data.sequences.slice( (page-1)*200, page*200)
-
-    api.data.current_page = parseInt(page)
-    api.data.number_of_sequences = api.data.sequences.length
-    return api
-}
-
-const getSequenceById = (id) => {
-    api.data.sequence = data.sequences.filter(seq => seq.promoter_id == id)
-    return api
-} 
 
 module.exports = router
---
+++ routes/api/models.js
@@ -0,0 +1,26 @@
+const data = require('../../lib/data/sequence_coords')
+const api = require('./sequences.json')
+
+exports.getSequences = () => {
+    delete api.data.sequence;
+    api.data.number_of_sequences = data.sequences.length
+    api.data.sequences = data.sequences.slice(0, data.sequences.length)
+    return api
+}
+
+exports.getSequencesPage = (page) => {
+    delete api.data.sequence;
+    if (page == 1) api.data.sequences = data.sequences.slice( 0, page*200)
+    else if (page == 10) api.data.sequences = data.sequences.slice( (page-1) *200)
+    else api.data.sequences = data.sequences.slice( (page-1)*200, page*200)
+
+    api.data.current_page = parseInt(page)
+    api.data.number_of_sequences = api.data.sequences.length
+    return api
+}
+
+exports.getSequenceById = async (id) => {
+    delete api.data.sequences;
+    api.data.sequence = await data.sequences.filter(seq => seq.promoter_id == id)
+    return api
+} 
